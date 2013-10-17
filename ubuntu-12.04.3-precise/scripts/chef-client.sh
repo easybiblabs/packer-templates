5e1d82d9cb31a48ba3952d3c5da6ea2b7179e4b4
fh <fh-github@fholzhauer.de>
2013-10-17 09:04:46 +0200

    upgrading to chef 11.4.4-2

--- ubuntu-12.04.3-precise/scripts/chef-client.sh
+++ ubuntu-12.04.3-precise/scripts/chef-client.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 
-# Cause Chef 11 breaks it all!
-CHEF_DEB=chef_10.24.0-1.ubuntu.10.04_amd64.deb
+#CHEF_DEB=chef_10.24.0-1.ubuntu.10.04_amd64.deb
+CHEF_DEB=chef_11.4.4-2.ubuntu.11.04_amd64.deb
 TARGET="/tmp/${CHEF_DEB}"
 
-wget -O "${TARGET}" "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/${CHEF_DEB}"
+wget -nc -O "${TARGET}" "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/${CHEF_DEB}"
 dpkg -i "${TARGET}"
 rm -f "${TARGET}"
