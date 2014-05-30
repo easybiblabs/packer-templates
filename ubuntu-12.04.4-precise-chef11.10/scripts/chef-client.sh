64595d776bd7c51c243572c7688b620287182558
Florian Holzhauer <fh-github@fholzhauer.de>
2014-05-30 15:26:51 +0200

    Adding 12.04.4 box with chef 11.10

---
+++ ubuntu-12.04.4-precise-chef11.10/scripts/chef-client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+CHEF_DEB=chef_11.10.4-1.ubuntu.11.04_amd64.deb
+TARGET="/tmp/${CHEF_DEB}"
+
+wget -nc -O "${TARGET}" "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/11.04/x86_64/${CHEF_DEB}"
+dpkg -i "${TARGET}"
+rm -f "${TARGET}"
---
+++ ubuntu-12.04.4-precise-chef11.10/scripts/minimize.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Credits to:
+#  - http://vstone.eu/reducing-vagrant-box-size/
+#  - https://github.com/mitchellh/vagrant/issues/343
+
+aptitude -y purge ri
+aptitude -y purge installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
+aptitude -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30
+aptitude -y purge python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
+
+#remove all but current
+dpkg -l 'linux-*' |grep ^ii
+uname -a
+apt-get remove -y --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
+apt-get remove -y linux-source*
+dpkg -l 'linux-*' |grep ^ii
+
+# Remove APT cache
+apt-get clean -y
+apt-get autoclean -y
+
+# Zero free space to aid VM compression
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Remove bash history
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+
+# Cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+# Whiteout root
+count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`; 
+let count--
+dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
+rm /tmp/whitespace;
+
+# Whiteout /boot
+count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+let count--
+dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+rm /boot/whitespace;
+
+swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
+swapoff $swappart;
+dd if=/dev/zero of=$swappart;
+mkswap $swappart;
+swapon $swappart;
