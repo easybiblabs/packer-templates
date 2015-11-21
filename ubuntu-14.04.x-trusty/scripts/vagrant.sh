290ebdca610c81bc8ca1810a9e7fead87b11c4ee
till <till@php.net>
2015-11-22 00:17:28 +0100

    Update: add a build for vmware
     * split of vagrant.sh into vagrant and virtualbox scripts
     * updated to packer 0.7.8

--- ubuntu-14.04.x-trusty/scripts/vagrant.sh
+++ ubuntu-14.04.x-trusty/scripts/vagrant.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -eux
 
-mkdir /tmp/vbox
-VER=$(cat /home/vagrant/.vbox_version)
-wget -nc http://download.virtualbox.org/virtualbox/$VER/VBoxGuestAdditions_$VER.iso
-mount -o loop VBoxGuestAdditions_$VER.iso /tmp/vbox
-sh /tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
-rmdir /tmp/vbox
-rm *.iso
-
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
