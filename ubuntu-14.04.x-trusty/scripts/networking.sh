ae2b2975f4ae69eff0795233c9e86f794f16485f
till <till@php.net>
2015-11-23 17:15:50 +0100

    Update: ensure settings are applied

--- ubuntu-14.04.x-trusty/scripts/networking.sh
+++ ubuntu-14.04.x-trusty/scripts/networking.sh
@@ -6,6 +6,10 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -rf /dev/.udev/ /var/lib/dhcp3/*
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
+# disable ipv6
 echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
 echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+
+# apply settings
+sysctl -p
