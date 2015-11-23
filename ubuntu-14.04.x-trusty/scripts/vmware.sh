b8f81b9505a9f289ff253a316e5668212dc12afe
till <till@php.net>
2015-11-23 20:52:56 +0100

    Fix: vmware tools (another attempt)

--- ubuntu-14.04.x-trusty/scripts/vmware.sh
+++ ubuntu-14.04.x-trusty/scripts/vmware.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -eux
 
-apt-get -y install open-vm-tools
-echo -n ".host:/ /mnt/hgfs vmhgfs rw,ttl=1,uid=my_uid,gid=my_gid,nobootwait 0 0" >> /etc/fstab
+mkdir /tmp/vmfusion
+mkdir /tmp/vmfusion-archive
+mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
+tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
+/tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
+umount /tmp/vmfusion
+rm -rf  /tmp/vmfusion
+rm -rf  /tmp/vmfusion-archive
+rm /home/vagrant/*.iso
