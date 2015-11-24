0d9a9b4eea91e7a4f41dcf5d8b61b7325d96b5d5
till <till@php.net>
2015-11-24 12:07:15 +0100

    Update: disable reboot

--- ubuntu-14.04.x-trusty/scripts/virtualbox.sh
+++ ubuntu-14.04.x-trusty/scripts/virtualbox.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -eux
 
-#we have to reboot here, so the re-build of the virtualbox additions links against the new kernel/headers
-reboot
-
-#make sure we wait for the reboot, otherwise we will run in ugly race conditions
-sleep 999999
+echo " -> download & install virtualbox guest additions"
 
 mkdir /tmp/vbox
 VER=$(cat /home/vagrant/.vbox_version)
